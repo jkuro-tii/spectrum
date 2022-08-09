f8535ec1658016d16f826f3a7f3f2ef485a70121
Jaroslaw Kurowski <jaroslaw.kurowski@tii.ae>
2022-08-09 13:36:46 +0400

    Removed unneeded cross compilation setting

--- vm/app/catgirl/shell.nix
+++ vm/app/catgirl/shell.nix
@@ -1,11 +1,7 @@
 # SPDX-License-Identifier: MIT
 # SPDX-FileCopyrightText: 2021 Alyssa Ross <hi@alyssa.is>
 
-{ pkgs ? import <nixpkgs> {
-    crossSystem = {
-      config = "aarch64-unknown-linux-musl";
-    };
-  }}:
+{ pkgs ? import <nixpkgs> {} }:
 
 with pkgs;
 
