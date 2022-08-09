739cc5a686ff035efb3a14bb4492ee3db3a8065e
Jaroslaw Kurowski <jaroslaw.kurowski@tii.ae>
2022-08-09 13:36:46 +0400

    aarch64: Removed unneeded cross compilation setting

--- host/initramfs/shell.nix
+++ host/initramfs/shell.nix
@@ -1,11 +1,7 @@
 # SPDX-License-Identifier: MIT
 # SPDX-FileCopyrightText: 2021-2022 Alyssa Ross <hi@alyssa.is>
 
-{ pkgs ? import <nixpkgs> {
- crossSystem = {
-      config = "aarch64-unknown-linux-musl";
-    };
-} }:
+{ pkgs ? import <nixpkgs> {} }:
 
 let
   inherit (pkgs.lib) cleanSource cleanSourceWith;
